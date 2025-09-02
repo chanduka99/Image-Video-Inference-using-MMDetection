75bedab300977e0ee97c387f75dbbb5f68305dcd
Kaveesha Chanduka <chanduka404@gmail.com>
2025-09-02 12:54:36 +0530

    add utils.parse_meta_file

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import os
 import requests
-
+import glob
+import yaml
 
 def download_weights(url,file_save_name):
     """
@@ -21,3 +22,30 @@ def download_weights(url,file_save_name):
         ).write(file.content)
 
 
+
+def parse_meta_file():
+    """
+    Function to parse all the model files inside 'mmdetection/configs'
+    and return the download URLs for all the available models.
+
+    Returns:
+        weight_list: List containing URLs for all the downloadble models.
+    """
+    root_meta_file_path='mmdetection/configs'
+    all_meta_file_paths=glob.glob(os.path.join(root_meta_file_path,"*","metafile.yml"),recursive=True)
+    weights_list = []
+
+    for meta_file_path in all_meta_file_paths:
+        with open(meta_file_path,'r') as f:
+            yaml_file = yaml.safe_load(f)
+
+            for i in range(len(yaml_file['Models'])):
+                try:
+                    weights_list.append(yaml_file['Models'][i]['Weights'])
+                except:
+                    for k,v in yaml_file['Models'][i]['Results'][0]['Metrics'].items():
+                        if k== 'Weights':
+                            weights_list.append(yaml_file['Models'][i]['Results'][0]['Metrics']['Weights'])
+    
+
+    return weights_list
